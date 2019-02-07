6a9fb43ebac0f58686d304c6b64cc78dbad6dbd7
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-07 17:21:58 +0100

    first commit, finished program

---
+++ pw.py
@@ -0,0 +1,23 @@
+#! python3
+#Project: password locker
+#Step 1: Program design and data structures
+
+#pw.py - An insecure password locker program
+
+PASSWORDS = {'email': 'fdwfrevrvfd',
+             'blog': 'fwrfervkn3475ygvnf',
+             'luggage': 'weih78238dehw8'}
+
+import sys, pyperclip
+if len (sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1] # first command line arg is the account name
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account + '.')
+    
